2daebad5554936828d40329ca8fddd0de2d6c699
Marvellous Adetunji <adetunjimarvellous09@gmail.com>
2023-07-11 16:50:48 +0100

    Created contact me form

---
+++ src/components/Contact.tsx
@@ -0,0 +1,144 @@
+"use client";
+import { Icon } from "@iconify/react";
+import React, { useReducer } from "react";
+
+const initialState = {
+  name: "",
+  email: "",
+  messsage: "",
+};
+
+type initialStateType = typeof initialState;
+
+type reducerAction = { input: string; value: string };
+
+const formReducer = (state: initialStateType, action: reducerAction) => {
+  return { ...state, [action.input]: action.value };
+};
+
+export default function Contact() {
+  const [state, dispatch] = useReducer(formReducer, initialState);
+
+  const handleChange = (
+    e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
+  ) => {
+    dispatch({ input: e.target.name, value: e.target.value });
+  };
+
+  const handleSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+    console.log(state);
+  };
+
+  return (
+    <div className="flex-between flex-col md:flex-row w-full" id="get-in-touch">
+      <div className="w-full md:w-[45%] flex flex-col items-center md:items-baseline gap-4 md:gap-8">
+        <h2 className="title_text uppercase blue_gradient_text">
+          Get in touch with me
+        </h2>
+        <p className="body_text">
+          Have something in mind? Looking to partner or work together? Fill in
+          the form and ill be sure to reach out
+        </p>
+        <ul className="flex-center gap-4">
+          <li>
+            <a
+              href=""
+              target="_blank"
+              className="cursor-pointer text-white-sec"
+            >
+              <Icon
+                icon="clarity:email-solid"
+                color="#fff"
+                height={32}
+                width={32}
+              />
+            </a>
+          </li>
+          <li>
+            <a
+              href=""
+              target="_blank"
+              className="cursor-pointer text-white-sec"
+            >
+              <Icon
+                icon="ic:baseline-whatsapp"
+                color="#fff"
+                height={32}
+                width={32}
+              />
+            </a>
+          </li>
+          <li>
+            <a
+              href=""
+              target="_blank"
+              className="cursor-pointer text-white-sec"
+            >
+              <Icon icon="mdi:twitter" color="#fff" height={32} width={32} />
+            </a>
+          </li>
+          <li>
+            <a
+              href=""
+              target="_blank"
+              className="cursor-pointer text-white-sec"
+            >
+              <Icon icon="mdi:linkedin" color="#fff" height={32} width={32} />
+            </a>
+          </li>
+          <li>
+            <a
+              href=""
+              target="_blank"
+              className="cursor-pointer text-white-sec"
+            >
+              <Icon icon="mdi:github" color="#fff" height={32} width={32} />
+            </a>
+          </li>
+        </ul>
+      </div>
+      <div className="w-full md:w-[45%]">
+        <form
+          className="flex items-stretch justify-center gap-4 md:gap-6 flex-col"
+          onSubmit={handleSubmit}
+        >
+          <label>
+            <span>Your Name</span>
+            <input
+              type="text"
+              title="name"
+              name="name"
+              value={state.name}
+              onChange={handleChange}
+            />
+          </label>
+          <label>
+            <span>Your Email</span>
+            <input
+              type="email"
+              title="email"
+              name="email"
+              value={state.email}
+              onChange={handleChange}
+            />
+          </label>
+          <label>
+            <span>What are you looking for</span>
+            <textarea
+              name="message"
+              title="message"
+              value={state.messsage}
+              onChange={handleChange}
+            />
+          </label>
+          <div>
+            <button className="pry_btn" onSubmit={handleSubmit}>
+              SUBMIT
+            </button>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+}
--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,56 +1,9 @@
-"use client";
-
-import { Icon } from "@iconify/react";
+import Contact from "./Contact";
 
 export default function Footer() {
   return (
     <footer className="w-full relative flex-center flex-col section border-t-2 border-black-sec">
-      <div className="flex-center flex-col gap-4 absolute animate-bounce-slow -top-2 mx-auto blue_gradient p-8 md:p-16 rounded-br-full rounded-bl-full">
-        <p className="text-white-pry title_text">
-          Interested in working with me ?
-        </p>
-        <span>
-          <Icon
-            icon="icon-park-solid:down-two"
-            color="white"
-            height={48}
-            width={48}
-          />
-        </span>
-      </div>
-      <div
-        className="flex-between flex-col md:flex-row w-full mt-64"
-        id="get-in-touch"
-      >
-        <div className="w-full md:w-[40%] flex flex-col items-center md:items-baseline gap-8">
-          <h2 className="title_text uppercase">Get in touch with me</h2>
-          <p className="body_text">
-            Have something in mind? Looking to partner or work together? Fill in
-            the form and ill be sure to reach out
-          </p>
-          <ul>
-            <li>
-              <a
-                href=""
-                className="flex gap-2 items-center py-2 text-white-sec"
-              >
-                <span>
-                  <Icon
-                    icon="clarity:email-solid"
-                    color="#fff"
-                    height={32}
-                    width={32}
-                  />
-                </span>
-                <span className="tiny_text">
-                  adetunjimarvellous09@gmail.com
-                </span>
-              </a>
-            </li>
-          </ul>
-        </div>
-        <div className="w-full md:w-[40%]"></div>
-      </div>
+      <Contact />
       <div></div>
     </footer>
   );

58c968948e74b4f365d8517e7d652b09dc32ea32
Marvellous Adetunji <adetunjimarvellous09@gmail.com>
2023-09-02 21:17:18 +0100

    maintenance

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -34,13 +34,22 @@ export default function Navbar() {
           <Link href={"/"}>Marvellous</Link>
         </p>
 
-        <ul className="">
-          <li className="px-6 py-2 rounded-2xl blue_gradient nav_link cursor-pointer">
-            <a title="download resume" href="../app/favicon.ico" download target="_blank" rel="noopener noreferrer">
-              Resume
-            </a>
-          </li>
-        </ul>
+
+        <div className="px-6 py-2 rounded-lg blue_gradient cursor-pointer">
+          <a title="download resume"
+            href="https://drive.google.com/file/d/12ySwyDCZ_BDr7ZevRgkQsJV4z0XNpDr2/view?usp=sharing"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="flex-center gap-2 nav_link"
+          >
+            <span>
+              <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24"><path fill="none" stroke="white" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 15v4a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2v-4m4-5l5 5l5-5m-5 5V3" />
+              </svg>
+            </span>
+            <span>Resume</span>
+          </a>
+        </div>
+
       </div>
     </nav>
   );

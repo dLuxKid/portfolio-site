5dda22a4a1d1749396281a2c45dccd84458d05f6
dLuxKid <adetunjimarvellous09@gmail.com>
2025-05-31 11:31:49 +0100

    fixes

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -7,7 +7,7 @@ export default function NotFound() {
       <div className="flex items-center justify-center size-full flex-col gap-12 md:gap-16 p-[5%] rounded-[20px]">
         <h1 className="bg-text font-black text-7xl md:text-9xl ">404</h1>
         <p className="text-lg sm:text-xl md:text-2xl font-semibold">
-          Oops! Looks like this page wandered off to a non-existent page.
+          Oops! Looks like you wandered off to a non-existent page.
         </p>
         <div className="max-w-xs mx-auto w-full">
           <Link href={"/"}>
